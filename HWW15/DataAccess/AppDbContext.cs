772dba9f2c9fb0d721bb33838ef89a9d870bb9ac
aliizadjoo <Ali.zadjoo@gmail.com>
2025-10-12 11:15:29 +0330

    modelBuilder for User

---
+++ HWW15/DataAccess/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using HWW15.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWW15.DataAccess
+{
+    public class AppDbContext :DbContext
+    {
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Reservation> Reservation { get; set; }
+        public DbSet<RoomDetail> RoomDetail { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=DESKTOP-M2BLLND\\SQLEXPRESS;Database=HotelReservationDb;Integrated Security=True;TrustServerCertificate=True;")
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(u=>u.Username).HasMaxLength(100).IsRequired();
+                entity.HasIndex(u => u.Username).IsUnique();
+                entity.Property(u => u.Password).HasMaxLength(100).IsRequired();
+                entity.Property(u => u.Role).HasConversion<string>();
+            });
+        }
+
+    }
+
+}
+
--- HWW15/Entities/HotelRoom.cs
+++ HWW15/Entities/HotelRoom.cs
@@ -9,7 +9,7 @@ namespace HWW15.Entities
     public class HotelRoom
     {
         public int Id { get; set; }
-        public string RoleNumber { get; set; }
+        public string RoomNumber { get; set; }
         public int Capacity { get; set; }
         public int PricePerNight { get; set; }
         public DateTime CreatedAt { get; set; }
